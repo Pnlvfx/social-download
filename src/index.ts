d7a6c29906ecb4e0884c3464bdbd74ac6c7a5bbe
simone <simonegauli@gmail.com>
2024-01-08 05:11:27 +0100

    o

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // https://github.com/TobyG74/tiktok-api-dl/tree/master/src
 export { default as insta } from './insta.js';
-export { default as tiktok } from './tiktok/tiktok.js';
+export { default as TiktokDL } from './tiktok/tiktok.js';
 
 // import coraline from 'coraline';
 // import { testTiktok } from './test/test-tiktok.js';
