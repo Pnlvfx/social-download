d197dbf23995e9b558f8a7ab9deab0565132c07a
simone <simonegauli@gmail.com>
2024-01-08 05:09:05 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 // https://github.com/TobyG74/tiktok-api-dl/tree/master/src
-
-import coraline from 'coraline';
-import { testTiktok } from './test/test-tiktok.js';
-
 export { default as insta } from './insta.js';
 export { default as tiktok } from './tiktok/tiktok.js';
 
-coraline.createScriptExec(testTiktok);
+// import coraline from 'coraline';
+// import { testTiktok } from './test/test-tiktok.js';
+
+// coraline.createScriptExec(testTiktok);
